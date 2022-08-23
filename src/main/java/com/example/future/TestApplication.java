d70751318d0b502b7acce52efb14ece1c9a0416a
MediaNik5 <stafilopok@mail.ru>
2022-08-23 20:38:03 +0300

    Simplify test

--- src/main/java/com/example/future/TestApplication.java
+++ src/main/java/com/example/future/TestApplication.java
@@ -1,86 +1,68 @@
 package com.example.future;
 
-import lombok.SneakyThrows;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-@SpringBootApplication
 public class TestApplication{
 
-    public static void main(String[] args){
-        SpringApplication.run(TestApplication.class, args);
-    }
-    private static final RestTemplate restTemplate = new RestTemplate();
-    static void makeRequest(){
-        restTemplate.execute("http://localhost:8080/", HttpMethod.GET, request -> {}, response -> null);
-    }
-}
+    public static void main(String[] args) throws ExecutionException, InterruptedException{
+        oneCompletableFuture();
+        oneCompletableFutureInsideAnother();
 
-@Component
-class Asyncer implements CommandLineRunner{
-    @Override
-    public void run(String... args) throws Exception{
+        // don't kill main thread, because other threads will not have enough
+        // time to finish
+        Thread.sleep(2000);
+    }
+
+    private static void oneCompletableFuture() throws InterruptedException, ExecutionException{
         long time = System.nanoTime();
         try{
-            System.out.println("Start 1");
-            CompletableFuture.runAsync(TestApplication::makeRequest).get(1, TimeUnit.SECONDS);
-            System.out.println("End 1");
+            System.out.println("1 started");
+            CompletableFuture.runAsync(() -> {
+                try{
+                    Thread.sleep(2000);
+                }catch(InterruptedException e){
+                    throw new RuntimeException(e);
+                }
+            }).get(1, TimeUnit.SECONDS);
+            System.out.println("1 completed successfully");
         }catch(TimeoutException e){
-            System.out.println("Timeout 1");
+            System.out.println("1 timed out");
         }
         long endTime = System.nanoTime();
-        System.out.println((endTime - time)/1_000_000 + " ms");
+        System.out.println("1 took " + (endTime - time)/1_000_000 + " ms");
     }
-}
-@Component
-class AsyncerInsideAnotherAsyncer implements CommandLineRunner{
 
-    @Override
-    public void run(String... args) throws Exception{
+    private static void oneCompletableFutureInsideAnother() throws InterruptedException, ExecutionException{
         long time = System.nanoTime();
         try{
-            System.out.println("Start 2");
+            System.out.println("2 started");
             CompletableFuture.runAsync(() -> {
                 long innerTime = System.nanoTime();
                 try{
-                    System.out.println("Start inner");
-                    CompletableFuture.runAsync(TestApplication::makeRequest).get(1, TimeUnit.SECONDS);
-                    System.out.println("End inner");
+                    System.out.println("inner started");
+                    CompletableFuture.runAsync(() -> {
+                        try{
+                            Thread.sleep(2000);
+                        }catch(InterruptedException e){
+                            throw new RuntimeException(e);
+                        }
+                    }).get(1, TimeUnit.SECONDS);
+                    System.out.println("inner completed successfully");
                 }catch(InterruptedException | ExecutionException | TimeoutException e){
-                    System.out.println("Inner timeout");
+                    System.out.println("inner timed out");
                 }
                 long innerTimeEnd = System.nanoTime();
-                System.out.println((innerTimeEnd - innerTime)/1_000_000 + " ms");
+                System.out.println("inner took " + (innerTimeEnd - innerTime)/1_000_000 + " ms");
             }).get(1, TimeUnit.SECONDS);
-            System.out.println("End 2");
+            System.out.println("2 completed successfully");
         }catch(TimeoutException e){
-            System.out.println("Timeout 2");
+            System.out.println("2 timed out");
         }
         long endTime = System.nanoTime();
-        System.out.println((endTime - time)/1_000_000 + " ms");
+        System.out.println("2 took " + (endTime - time)/1_000_000 + " ms");
     }
 }
 
-
-
-
-@RestController
-class ControllerWithHighWork{
-    @SneakyThrows
-    @GetMapping
-    public String get(){
-        Thread.sleep(2000);
-        return "Hello";
-    }
-}
